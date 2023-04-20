ede20420aaf852bf325ed032eb04b5f3b9df9a7c
kaitlyn-crutcher <crutchkb@mail.uc.edu>
2023-04-20 13:07:27 -0400

    commit

---
+++ js/product.js
@@ -0,0 +1,12 @@
+	
+$(document).ready(function() {
+ 
+    $('.color-choose input').on('click', function() {
+        var cartColor = $(this).attr('data-image');
+   
+        $('.active').removeClass('active');
+        $('.below img[data-image = ' + cartColor + ']').addClass('active');
+        $(this).addClass('active');
+    });
+   
+  });
