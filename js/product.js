9b3e56471e9ced38d007e7cd34a5b3ece5456da5
Dave Berning <daveberning@outlook.com>
2023-04-20 18:22:47 -0400

    converted some radio buttons to li items

--- js/product.js
+++ js/product.js
@@ -1,6 +1,6 @@
 	
 $(document).ready(function() {
-    $('.color-choose input').on('click', function() {
+    $('.color-choose li').on('click', function() {
         const cartColor = $(this).attr('data-image');
         $('#product-image').attr('src', "../img/products/beyondfour/" + cartColor + ".jpg");
     });
